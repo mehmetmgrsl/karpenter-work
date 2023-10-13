24ad7ce34ee66af86d1a8d4e6c115a94446a388d
mehmetmgrsl <mehmetgursul@gmail.com>
2023-10-13 15:29:41 +0200

    Create AWS VPC

---
+++ 00-provider.tf
@@ -0,0 +1,22 @@
+variable "cluster_name" {
+  default = "test"
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+  }
+}
